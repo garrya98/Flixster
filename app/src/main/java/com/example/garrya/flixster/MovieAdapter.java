5eec2884a8a5371831c4bfb28d67f5e98b308a7d
Garry Jamel Archbold Humphries <garrya@fb.com>
2017-06-22 17:08:20 -0700

    initial commit

---
+++ app/src/main/java/com/example/garrya/flixster/MovieAdapter.java
@@ -0,0 +1,15 @@
+package com.example.garrya.flixster;
+
+import com.example.garrya.flixster.Models.Movie;
+
+import java.util.ArrayList;
+
+/**
+ * Created by garrya on 6/22/17.
+ */
+
+public class MovieAdapter {
+
+    // list of movies
+    ArrayList<Movie> movies;
+}
